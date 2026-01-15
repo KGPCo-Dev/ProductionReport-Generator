dbf7e2e0d36edb294a5638fe9e27d53a2c606069
Mauricio Márquez <Jose.Marquez@kgpco.com>
2026-01-15 11:58:14 -0600

    Order Status and DynamicTables adjusted.
    - table_loader_script.html was created
    - ORDER_STATUS_QUERY adjusted to dispplay correct hour format.
    
    TODO
    - Add CCS
    - Add Angular

--- production_report/order_tracker/views.py
+++ production_report/order_tracker/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from django.db import connection
+from reports.queries import REPORT_CONFIG
+
+
+def dicfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [ 
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+     ]
 
 def order_tracker_view(request):
 
     build_id = request.GET.get('search')
+    report_type = request.GET.get('report_type', 'order_status_report')
+    results = None
+    headers = None
+
+    if request.method == 'GET':
+        if build_id:
+
+            config = REPORT_CONFIG.get(report_type, REPORT_CONFIG['order_status_report'])
+            query = config['query']
+
+            with connection.cursor() as cursor:
+                cursor.execute(query, [build_id])
+                results = dicfetchall(cursor)
+
+                if results:
+                    headers = [col[0] for col in cursor.description]
 
     return render(request,'order_tracker/order_tracker_preview.html', { 
-        'build_id': build_id
+        'build_id': build_id,
+        'results': results,
+        'headers': headers,
+        'report_type': report_type
      })
--- production_report/reports/queries.py
+++ production_report/reports/queries.py
@@ -1,3 +1,23 @@
+ORDER_STATUS_QUERY = """
+SELECT
+    build_id as "Orden",
+    entered_date::DATE AS "Fecha",
+    TO_CHAR(entered_date::TIME, 'HH24:MI') AS "Hora",
+    employee_number AS "Empleado",
+    workplace AS "Estacion",
+    production_cell AS "Celda",
+    production_shift AS "Turno",
+    tethers_completed || ' de ' || tethers_total AS "Progreso de Tethers",
+    build_attempt AS "Intentos",
+    result_status AS "Estatus"
+FROM public.kgp_test2_results
+WHERE build_id = %s
+    AND result_status IS NOT NULL
+    AND workplace IS NOT NULL
+ORDER BY entered_date ASC
+"""
+
+
 SCRAP_REPORT_QUERY = """
 SELECT
     results.entered_date AS "Fecha de Registro",
@@ -50,7 +70,7 @@ JOIN public.kgp_production_orders orders ON results.build_id = orders.build_id
 WHERE results.entered_date >= (%s::DATE + INTERVAL '7 hours')
     AND results.entered_date < (%s::DATE + INTERVAL '1 day' + INTERVAL '7 hours')
     AND results.workplace IS NOT NULL
-    AND results.result_status != 'Rework'
+    AND results.result_status IS DISTINCT  FROM 'Rework'
     {shift_clause}
 ORDER BY results.entered_date;
 """
@@ -65,5 +85,10 @@ REPORT_CONFIG = {
         'query': PRODUCTION_REPORT_QUERY,
         'filename': 'Reporte de Produccion',
         'sheet_name': 'Produccion'
-      }
+      },
+      'order_status_report': { 
+        'query': ORDER_STATUS_QUERY,
+        'filename': 'Estatus de Orden',
+        'sheet_name' : 'Order_Status'
+       },
  }
