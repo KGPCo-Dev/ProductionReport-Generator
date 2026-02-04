b2f70c078c3a106bf51f65e600458fab505f7031
Mauricio Márquez <Jose.Marquez@kgpco.com>
2026-02-04 17:03:19 -0600

    dawdawd
    fafe

--- production_report/order_tracker/tests.py
+++ production_report/order_tracker/tests.py
@@ -31,6 +31,13 @@ order_results_query =  ORDER_RESULTS_QUERY
 order_fails_results_query = ORDER_FAIL_RESULTS_QUERY
 build_id = "CCS1487777"
 
+with connection.cursor() as order_fails_cursor: 
+    order_fails_cursor.execute(order_fails_results_query, [build_id])
+    order_fails_results = dictfetchall(order_fails_cursor)
+
+    if order_fails_results:
+        order_fails_results_headers = [col[0] for col in order_fails_cursor.description]
+
 with connection.cursor() as order_results_cursor:
     order_results_cursor.execute(order_results_query, [build_id])
     order_results = dictfetchall(order_results_cursor)
@@ -40,11 +47,17 @@ with connection.cursor() as order_results_cursor:
     else:
         order_results_headers = "No value"
 
-with connection.cursor() as order_fails_cursor: 
-    order_fails_cursor.execute(order_fails_results_query, [build_id])
-    order_fails_results = dictfetchall(order_fails_cursor)
-
-    if order_fails_results:
-        order_fails_results_headers = [col[0] for col in order_fails_cursor.description]
-print("order_results value: ", order_results[0], order_results[1])
+for row in order_results:
+    row['fails'] = [ 
+        { 
+            'Descripcion': fail['fail_description'],
+            'Intentos': fail['fail_amount']
+        }
+         for fail in order_fails_results
+        if fail['process_id'] == row['process_id']
+        and fail['global_tether'] == row['Numero de Tether']
+     ]
+print("order_results value: ", order_results[2], order_results[3])
 print("order_results_headers value: ", order_results_headers)
+
+print("order_fail_results value: ", order_fails_results[2], order_fails_results[3])
--- production_report/order_tracker/views.py
+++ production_report/order_tracker/views.py
@@ -49,22 +49,23 @@ def get_process_results(request, build_id):
     report_type = request.GET.get('report_type', 'order_process_results')
     print("report_type value on process_results:", report_type)
 
+    # DB connection and config is created #
     config = REPORT_CONFIG.get(report_type, REPORT_CONFIG['order_process_results'])
     query = config['query']
-    
+    fails_results_query = ORDER_FAIL_RESULTS_QUERY
     results = []
     headers = []
 
-    fails_reuslts_query = ORDER_FAIL_RESULTS_QUERY
+    
 
     with connection.cursor() as fails_results_cursor:
-        fails_results_cursor.execute(fails_reuslts_query, [build_id])
+        fails_results_cursor.execute(fails_results_query, [build_id])
         fails_results = dicfetchall(fails_results_cursor)
 
-        print("Fail results from DB: ", fails_results)
 
         if fails_results:
             fails_headers = [col[0] for col in fails_results_cursor.description]
+
 
     with connection.cursor() as process_results_cursor:
         process_results_cursor.execute(query, [build_id])
@@ -73,23 +74,42 @@ def get_process_results(request, build_id):
         if results:
             headers = [col[0] for col in process_results_cursor.description]
 
-            # ProcesName is assigned isted of use proces.id number #
+            # ProcesName is assigned insted of use proces.id number #
             for row in results:
                 original_name = row['Proceso']
                 try:
                     row['Proceso'] = ProcessNames(original_name).label
                 except ValueError:
                     row['Proceso'] = f"Desconocido ({ original_name })"
+            
+            # fails Column is created and a list of dict is attatched to it #
+            for row in results:
+                row['fails'] = [ 
+                    { 
+                        'Descripcion': fail['fail_description'],
+                        'Intentos': fail['fail_amount']
+                    }
+                    for fail in fails_results 
+                    if fail['process_id'] == row['process_id'] 
+                    and fail['global_tether'] == row['Numero de Tether']
+                ]
 
-    
+    # Delete process_id value from headers if it exists #
+    if results:
+        headers_process= headers.pop(0)
+
+    print("Resultados en views: ", results)
+    print("Headers en views: ", headers)
+
+
     process_results = [results, headers]
-    print("Headers on process_results", headers)
+
+
     return process_results
 
 def get_test2_results(request, build_id):
 
     report_type = request.GET.get('report_type', 'order_status_results')
-    print("report_type value on test2_results:", report_type)
 
     config = REPORT_CONFIG.get(report_type, REPORT_CONFIG['order_status_results'])
     query = config['query']
@@ -105,5 +125,5 @@ def get_test2_results(request, build_id):
             headers = [col[0] for col in cursor.description]
 
     test2_results = [results, headers]
-    print("Headers on test2_results", headers)
+
     return test2_results
